82e19e0eba6a28f23dde7670d680a3a14ebee3eb
Levi Schuck <me@levischuck.com>
2026-01-07 23:17:55 -0600

    fix: Build tiny-png type-check

--- packages/tiny-qr-png/vite.config.ts
+++ packages/tiny-qr-png/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   },
   plugins: [
     dts({
-      outDir: 'dist',
+      outDirs: 'dist',
       entryRoot: 'src',
       include: ['src/**/*.ts'],
       exclude: ['src/**/*.test.ts']
